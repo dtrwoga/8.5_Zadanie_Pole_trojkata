a9fa6f08199bf05a25f1ec899f935a29ca4f708c
dtrwoga <dtrwoga@gmail.com>
2017-07-31 01:54:40 +0200

    not working console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 // scripts.js
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a = prompt('Enter the base a');
+var h = prompt('Enter the height h');
+var triangleArea = a*h/2
+
+if (isNaN(a) || isNaN(h) ){
+	alert('At least one of the variables is not a number, enter the correct values');
+}
+else {
+	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	document.write('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+}
+
+
+
+
+
